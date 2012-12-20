326ba7f4f4daf56577b7d908282e030d89d07a19
esterren <renato.estermann@gmail.com>
2012-12-20 14:16:12 +0100

    map

--- assets/www/js/trackRoute.js
+++ assets/www/js/trackRoute.js
@@ -4,14 +4,15 @@
     //
     function onTrackRoute() {
 	
-		 var latlng = new google.maps.LatLng(57.7973333, 12.0502107);
-            var myOptions = {
-                zoom: 8,
-                center: latlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-		var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
-
+		//var latlng = new google.maps.LatLng(57.7973333, 12.0502107);
+        //var myOptions = {
+        //        zoom: 8,
+        //        center: latlng,
+        //        mapTypeId: google.maps.MapTypeId.ROADMAP
+        //    };
+		//var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+		var yourStartLatLng = new google.maps.LatLng(59.3426606750, 18.0736160278);
+        $('#map_canvas').gmap({'center': yourStartLatLng});
 		//$('#map_canvas').gmap('addMarker', {'position': '57.7973333,12.0502107', 'bounds': true}).click(function() {
      	//$('#map_canvas').gmap('openInfoWindow', {'content': 'Hello World!'}, this);
         //});
